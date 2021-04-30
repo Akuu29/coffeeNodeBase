b93a847411d6214d8290e6ef8f9eb528885186d7
Akuu29 <owner.akiu0529@gmail.com>
2021-04-30 23:06:06 +0900

    UPDATE When the user logged in or signed up , the function into which a session is formed is made.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
+// import cookieParser from "cookie-parser";
 
 require("dotenv").config();
 
@@ -18,6 +19,7 @@ const con = mysql.createConnection({
 	database: process.env.DB_NAME
 });
 
+// セッション生成用DB情報
 const sessOption = {
 	host: "localhost",
 	user: "root",
@@ -27,10 +29,10 @@ const sessOption = {
 
 const sessionStore = new MySQLStore(sessOption);
 
-con.connect(function(err: any) {
-	if(err) throw err;
-	console.log("Connected");
-});
+// con.connect(function(err: any) {
+// 	if(err) throw err;
+// 	console.log("Connected");
+// });
 
 // セッション生成
 const sess = {
@@ -43,13 +45,13 @@ const sess = {
 };
 
 if(app.get("env") == "production") {
-	app.set("trust proxy", 1)
+	app.set("trust proxy", 1);
 	// sess.cookie.secure = true
 }
 
 app.use(session(sess));
-
 app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
 
 // View Engineにejsを指定
 app.set("view engine", "ejs");
@@ -60,7 +62,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	// res.sendFile(`${__dirname}/views/index.ejs`);
-	res.render("index", {});
+	res.render("index", {nickname: ""});
 });
 
 app.get("/login", (req, res) => {
@@ -69,20 +71,31 @@ app.get("/login", (req, res) => {
 
 // ログイン
 app.post("/login", (req: any,res: any) => {
-	let loginNickname: string = req.body.nickname;
-	let loginPassword: string = req.body.password;
+	con.connect(function(err: any) {
+		if(err) throw err;
+		console.log("Connected");
+	});	
+
+	let loginUser: any = req.body;
+	let loginNickname: string = loginUser.nickname;
+	let loginPassword: string = loginUser.password;
 
 	let selectUsers: string = "SELECT NICKNAME, PASSWORD FROM USERS WHERE NICKNAME = ? AND PASSWORD = ?";
 	let usersInfo: any = [loginNickname, loginPassword];
 
 	// con.query(selectUsers, loginNickname, loginPassword, function(error: any, responce: any) {
 	con.query(selectUsers, usersInfo, function(error: any, results: any, fields: any) {
-		if(error) throw error;
+		if(error) {
+			throw error
+		};
 		// console.log(results);
 		if(results != "") {
 			req.session.regenerate((err: any) => {
-				req.session.nickname = loginNickname; // セッション生成
-				res.render("index", {});
+				req.session.loginUser = {
+					nickname: loginUser.nickname
+				}; // セッション生成
+				// console.log(req.session.loginUser.nickname);
+				res.render("index", {nickname: req.session.loginUser.nickname}); // nickname: session.nickname
 			});
 			
 		}else{
@@ -98,21 +111,36 @@ app.get("/signup", (req, res) => {
 	res.render("signup", {});
 });
 
-app.post("/signup", (req, res) => {
-	let signupNickname: string = req.body.nickname;
-	let signupMailaddress: string = req.body.mailaddress;
-	let signupPassword: string = req.body.password;
-	let signupConfirm: string = req.body.confirm;
+app.post("/signup", (req: any, res: any) => {
+	con.connect(function(err: any) {
+		if(err) throw err;
+		console.log("Connected");
+	});
+
+	let signupUser: any = req.body;
+	// let signupNickname: string = signupUser.nickname;
+	// let signupMailaddress: string = signupUser.mailaddress;
+	// let signupPassword: string = signupUser.password;
+	// let signupConfirm: string = signupUser.confirm;
 
 	// password欄confirm欄入力チェック 一致したら登録
-	if(signupPassword == signupConfirm) {
+	if(signupUser.password == signupUser.confirm) {
 		let insertUsers: string = "INSERT INTO USERS SET ?";
-		let post: any = {"nickname": signupNickname, "mail_address": signupMailaddress, "password": signupPassword};
+		let post: any = {"nickname": signupUser.nickname, "mail_address": signupUser.mailaddress, "password": signupUser.password};
 
 		con.query(insertUsers, post, function(error: any, results: any, fields: any) {
-			if(error) throw error;
-			console.log(results);
-			res.render("index", {});
+			if(error) {
+				throw error;
+			}
+			// console.log(results);
+			if(results != "") {
+				req.session.regenerate((err: any) =>{
+					req.session.signupUser = {
+						nickname: signupUser.nickname
+					};
+					res.render("index", {nickname: req.session.signupUser.nickname});
+				});
+			}
 		});
 
 	}else{
@@ -120,8 +148,6 @@ app.post("/signup", (req, res) => {
 	}
 
 });
-
-
 
 app.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
