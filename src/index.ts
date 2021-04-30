57dbf3ddbb959053bb608ba810143119fa681cce
Akuu29 <owner.akiu0529@gmail.com>
2021-04-30 23:36:28 +0900

    UPDATE Delete unneccesary commentout.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import session from "express-session";
-// import cookieParser from "cookie-parser";
 
 require("dotenv").config();
 
@@ -29,39 +28,29 @@ const sessOption = {
 
 const sessionStore = new MySQLStore(sessOption);
 
-// con.connect(function(err: any) {
-// 	if(err) throw err;
-// 	console.log("Connected");
-// });
-
 // セッション生成
 const sess = {
 	secret: "secretSecretSecret",
 	resave: false,
 	saveUninitialized: true,
 	cookie: {maxAge: 10 * 1000},
-	// cookie: {},
 	store: sessionStore
 };
 
 if(app.get("env") == "production") {
 	app.set("trust proxy", 1);
-	// sess.cookie.secure = true
 }
 
 app.use(session(sess));
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cookieParser());
 
 // View Engineにejsを指定
 app.set("view engine", "ejs");
 
 // 静的ファイルの提供
 app.use(express.static("public"));
-// app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	// res.sendFile(`${__dirname}/views/index.ejs`);
 	res.render("index", {nickname: ""});
 });
 
@@ -83,21 +72,19 @@ app.post("/login", (req: any,res: any) => {
 	let selectUsers: string = "SELECT NICKNAME, PASSWORD FROM USERS WHERE NICKNAME = ? AND PASSWORD = ?";
 	let usersInfo: any = [loginNickname, loginPassword];
 
-	// con.query(selectUsers, loginNickname, loginPassword, function(error: any, responce: any) {
 	con.query(selectUsers, usersInfo, function(error: any, results: any, fields: any) {
 		if(error) {
-			throw error
-		};
-		// console.log(results);
+			throw error;
+		}
+
 		if(results != "") {
 			req.session.regenerate((err: any) => {
 				req.session.loginUser = {
 					nickname: loginUser.nickname
 				}; // セッション生成
-				// console.log(req.session.loginUser.nickname);
+
 				res.render("index", {nickname: req.session.loginUser.nickname}); // nickname: session.nickname
-			});
-			
+			});	
 		}else{
 			// ユーザネームまたはパスワードが間違っています。
 			
@@ -118,10 +105,6 @@ app.post("/signup", (req: any, res: any) => {
 	});
 
 	let signupUser: any = req.body;
-	// let signupNickname: string = signupUser.nickname;
-	// let signupMailaddress: string = signupUser.mailaddress;
-	// let signupPassword: string = signupUser.password;
-	// let signupConfirm: string = signupUser.confirm;
 
 	// password欄confirm欄入力チェック 一致したら登録
 	if(signupUser.password == signupUser.confirm) {
@@ -131,8 +114,9 @@ app.post("/signup", (req: any, res: any) => {
 		con.query(insertUsers, post, function(error: any, results: any, fields: any) {
 			if(error) {
 				throw error;
+				// すでに登録済であることを表示
+
 			}
-			// console.log(results);
 			if(results != "") {
 				req.session.regenerate((err: any) =>{
 					req.session.signupUser = {
